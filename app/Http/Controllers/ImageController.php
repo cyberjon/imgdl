8e06da0f6976afe9310559cd3632f34a8d4d9f39
cyberjon <cyberjon09@gmail.com>
2022-06-12 22:48:10 +0600

    Image controller init

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Image;
+
+class ImageController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $images = Image::orderBy('created_at', 'desc');
+        $total_images = $images->get()->count('id');
+        $images = $images->paginate(20);
+        return view('images.index', compact('images','total_images'));
+    }
+
+    public function create()
+    {
+        return view('images.create');
+    }
+
+    public function upload()
+    {
+        return view('images.upload');
+    }
+
+    public function storeUpload(Request $request)
+    {
+        Image::create($request->all());
+        return redirect()->route('images.index');
+    }
+
+    public function storeDownload(Request $request)
+    {
+        Image::create($request->all());
+        return redirect()->route('images.index');
+    }
+
+    public function show(Request $request)
+    {
+        $id = $request->get('id');
+        $image = Image::where('id', $id)->get();
+        echo json_encode($image);
+    }
+
+    public function destroy(Image $image)
+    {
+        $image->delete();
+        return back();
+    }
+
+}
