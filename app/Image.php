c00971ba22d7f7e0091e3b4e5de1f373e0128fc9
cyberjon <cyberjon09@gmail.com>
2022-06-12 22:20:46 +0600

    Image model and migration

---
+++ app/Image.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'images';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'image_file',
+        'brand',
+        'camera',
+        'software',
+        'size',
+        'width',
+        'height',
+        'aperture',
+        'shutter_speed',
+        'iso',
+        'focal_length',
+        'lens',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
---
+++ database/migrations/2022_06_12_000000_create_images_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_file')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('camera')->nullable();
+            $table->string('software')->nullable();
+            $table->string('size')->nullable();
+            $table->string('width')->nullable();
+            $table->string('height')->nullable();
+            $table->string('aperture')->nullable();
+            $table->string('shutter_speed')->nullable();
+            $table->string('iso')->nullable();
+            $table->string('focal_length')->nullable();
+            $table->string('lens')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
