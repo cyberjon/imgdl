5fc1f2f92ada604ebc43269ba4caa2839559bd33
cyberjon <cyberjon09@gmail.com>
2022-06-13 14:08:30 +0600

    added button class

--- resources/views/images/download.blade.php
+++ resources/views/images/download.blade.php
@@ -5,7 +5,7 @@
             echo Form::open(array('url' => '/download','files'=>'true'));
             echo 'Enter image URL to upload.';
             echo Form::text('url', '', ['required']);
-            echo Form::submit('Save Image');
+            echo Form::submit('Save Image', ['class' => 'btn btn-primary']);
             echo Form::close();
         ?>
     </div>
--- resources/views/images/upload.blade.php
+++ resources/views/images/upload.blade.php
@@ -5,7 +5,7 @@
             echo Form::open(array('url' => '/upload','files'=>'true'));
             echo 'Select the file to upload.';
             echo Form::file('image', ['required']);
-            echo Form::submit('Upload Image');
+            echo Form::submit('Upload Image', ['class' => 'btn btn-primary']);
             echo Form::close();
         ?>
     </div>
